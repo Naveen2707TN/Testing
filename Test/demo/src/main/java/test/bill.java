010fa22fdf24a07eb3b16a76222170a7668e72a6
Naveen2707TN <naveenvishnu1007@gmail.com>
2024-06-21 07:47:48 +0530

    java line change

--- Test/demo/src/main/java/test/bill.java
+++ Test/demo/src/main/java/test/bill.java
@@ -1,11 +1,15 @@
 package main.java.test;
 
+import java.time.LocalDate;
 import java.util.Scanner;
+
+import javax.xml.crypto.Data;
 
 import com.example.*;
 
 public class bill {
     public int unit;
+    LocalDate lDate;
 
     public void Unit(){
         System.out.println("Enter the usage of unit : ");
@@ -14,15 +18,19 @@ public class bill {
         if (unit < 100) {
             int cal = unit * 4;
             System.out.println("To pay ammount is : "+cal);
+            System.out.println(lDate.now());
         }else if(unit == 100 && unit < 200){
             int cal = unit * 5;
             System.out.println("To pay ammount is : "+cal);
+            System.out.println(lDate.now()+);
         }else if (unit == 200 && unit < 300) {
             int cal = unit * 6;
             System.out.println("To pay ammount is : "+cal);
+            System.out.println(lDate.now());
         }else{
             int cal = unit * 7;
             System.out.println("To pay ammount is : "+cal);
+            System.out.println(lDate.now());
         }
     }
 
